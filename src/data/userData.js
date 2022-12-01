4f2f56fd0981df5a468949c09b92742dc7c355a5
Carlos Andrés Arévalo Torres <carlosandresat@hotmail.com>
2022-11-30 23:43:36 -0500

    userData file added

---
+++ src/data/userData.js
@@ -0,0 +1,56 @@
+export default [
+    {
+        user: 'carlos.arevalo',
+        password: 'carlos.arevalo',
+        userFirstName: 'Carlos',
+        tutorias: [
+            {
+                asignatura: 'Cálculo II',
+                fecha: '12/Nov',
+                hora: '08:00'
+            },
+            {
+                asignatura: 'Física I',
+                fecha: '12/Nov',
+                hora: '12:00'
+            },
+            {
+                asignatura: 'Química I',
+                fecha: '14/Nov',
+                hora: '10:00'
+            },
+            {
+                asignatura: 'Algoritmos',
+                fecha: '15/Nov',
+                hora: '14:00'
+            },
+        ],
+    },
+    {
+        user: 'selena.jimenez',
+        password: 'selena.jimenez',
+        userFirstName: 'Selena',
+        tutorias: [
+            {
+                asignatura: 'Hola, gracias',
+                fecha: '12/Nov',
+                hora: '08:00'
+            },
+            {
+                asignatura: 'Por curiosear',
+                fecha: '12/Nov',
+                hora: '12:00'
+            },
+            {
+                asignatura: 'Te quiero mucho',
+                fecha: '14/Nov',
+                hora: '10:00'
+            },
+            {
+                asignatura: 'Ex preciosa',
+                fecha: '15/Nov',
+                hora: '14:00'
+            },
+        ],
+    },
+]
